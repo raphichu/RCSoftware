36f884e585d75d163781f92b14b065cb6d6dd052
rapha <rapha@RCPG-Note>
2019-07-25 11:20:15 -0300

    Refatorados nomes de arquivos

--- Sistema/public_html/script.js
+++ Sistema/public_html/script.js
@@ -1,7 +1,10 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(".tile").mousedown(function () {
+    $(this).addClass("selecionado");
+});
 
-
+$(".tile").mouseup(function () {
+    $(this).removeClass("selecionado");
+});
+$(".tile").mouseleave(function () {
+    $(this).removeClass("selecionado");
+});
